8b318f84b4623a4e33831a147ff404504a40e47f
Nahuel Garbezza <n.garbezza@gmail.com>
2015-06-24 12:52:23 -0300

    caja de ahorros - test 03 en rojo

--- test/caja_de_ahorros_test.rb
+++ test/caja_de_ahorros_test.rb
@@ -21,9 +21,26 @@ class CajaDeAhorrosTest < Test::Unit::TestCase
   #     unaCajaDeAhorros := CajaDeAhorros new.
   #     unaCajaDeAhorros depositar: 100 pesos.
   #     self assert: unaCajaDeAhorros saldo = 100 pesos
+  #
   def test_02_dada_una_caja_de_ahorros_nueva_si_deposito_100_pesos_su_nuevo_saldo_debe_ser_100_pesos
     una_caja_de_ahorros = CajaDeAhorros.new
     una_caja_de_ahorros.depositar 100.pesos
+    self.assert_equal una_caja_de_ahorros.saldo, 100.pesos
+  end
+
+  # test03DadaUnaCajaDeAhorrosCuyoSaldoEs200pesosSiExtraigo100pesosSuNuevoSaldoDebeSer100pesos
+  #
+  #     | unaCajaDeAhorros |
+  #
+  #     unaCajaDeAhorros := CajaDeAhorros new.
+  #     unaCajaDeAhorros depositar: 200 pesos.
+  #     unaCajaDeAhorros extraer: 100 pesos.
+  #     self assert: unaCajaDeAhorros saldo = 100 pesos
+  #
+  def test_03_dada_una_caja_de_ahorros_cuyo_saldo_es_200_pesos_si_extraigo_100_pesos_su_nuevo_saldo_debe_ser_100_pesos
+    una_caja_de_ahorros = CajaDeAhorros.new
+    una_caja_de_ahorros.depositar 200.pesos
+    una_caja_de_ahorros.extraer 100.pesos
     self.assert_equal una_caja_de_ahorros.saldo, 100.pesos
   end
 
