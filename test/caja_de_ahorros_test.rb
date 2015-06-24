372f39053592beb75acfc8bf50705b8af7345454
Nahuel Garbezza <n.garbezza@gmail.com>
2015-06-24 12:50:23 -0300

    caja de ahorros - test 02 en rojo

--- test/caja_de_ahorros_test.rb
+++ test/caja_de_ahorros_test.rb
@@ -11,8 +11,20 @@ class CajaDeAhorrosTest < Test::Unit::TestCase
   #
   def test_01_dada_una_caja_de_ahorros_nueva_su_saldo_inicial_debe_ser_0_pesos
     una_caja_de_ahorros = CajaDeAhorros.new
-    assert_equal una_caja_de_ahorros.saldo, 0.pesos
+    self.assert_equal una_caja_de_ahorros.saldo, 0.pesos
   end
 
+  # test02DadaUnaCajaDeAhorrosNuevaSiDeposito100pesosSuNuevoSaldoDebeSer100pesos
+  #
+  #     | unaCajaDeAhorros |
+  #
+  #     unaCajaDeAhorros := CajaDeAhorros new.
+  #     unaCajaDeAhorros depositar: 100 pesos.
+  #     self assert: unaCajaDeAhorros saldo = 100 pesos
+  def test_02_dada_una_caja_de_ahorros_nueva_si_deposito_100_pesos_su_nuevo_saldo_debe_ser_100_pesos
+    una_caja_de_ahorros = CajaDeAhorros.new
+    una_caja_de_ahorros.depositar 100.pesos
+    self.assert_equal una_caja_de_ahorros.saldo, 100.pesos
+  end
 
 end
