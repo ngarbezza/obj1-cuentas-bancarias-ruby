2f4b3fc6c5e1e64bcdffd3596051ff2447a8778f
Nahuel Garbezza <n.garbezza@gmail.com>
2015-06-24 12:53:59 -0300

    caja de ahorros - test 04 en rojo

--- test/caja_de_ahorros_test.rb
+++ test/caja_de_ahorros_test.rb
@@ -44,4 +44,20 @@ class CajaDeAhorrosTest < Test::Unit::TestCase
     self.assert_equal una_caja_de_ahorros.saldo, 100.pesos
   end
 
+  # test04DadaUnaCajaDeAhorrosCuyoSaldoEs200pesosSiExtraigo300pesosSuSaldoNoDebeVariar
+  #
+  #     | unaCajaDeAhorros |
+  #
+  #     unaCajaDeAhorros := CajaDeAhorros new.
+  #     unaCajaDeAhorros depositar: 200 pesos.
+  #     unaCajaDeAhorros extraer: 300 pesos.
+  #     self assert: unaCajaDeAhorros saldo = 200 pesos
+  #
+  def test_04_dada_una_caja_de_ahorros_cuyo_saldo_es_200_pesos_si_extraigo_300_pesos_su_saldo_no_debe_variar
+    una_caja_de_ahorros = CajaDeAhorros.new
+    una_caja_de_ahorros.depositar 200.pesos
+    una_caja_de_ahorros.extraer 300.pesos
+    self.assert_equal una_caja_de_ahorros.saldo, 200.pesos
+  end
+
 end
